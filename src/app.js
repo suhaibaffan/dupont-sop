bb4b71e8fdf064959f10b485f52ec2081d8043d8
suhaibaffan <suhaibaffan91@gmail.com>
2018-05-28 00:25:09 +0530

    Added server port and index html.

--- src/app.js
+++ src/app.js
@@ -9,3 +9,7 @@ app.use( bodyParser.json() );
 app.get( '/', ( req, res ) => {
     res.sendFile(path.join(__dirname+'/views/index.html'));
 });
+
+let server = app.listen( 8000, () => {
+    console.log( 'Server listening on port 8000' );
+});
